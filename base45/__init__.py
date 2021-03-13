6edf4651b6c5f4e275aafe5397bbb8e7631dfdb5
Jakob Schlyter <jakob@kirei.se>
2021-03-13 23:21:21 +0100

    make 25% faster

--- base45/__init__.py
+++ base45/__init__.py
@@ -5,18 +5,18 @@ Base45 Data Encoding as described in draft-faltstrom-base45-02
 BASE45_CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:"
 
 
-def b45encode(s: bytes) -> str:
+def b45encode(buf: bytes) -> str:
     """Convert bytes to base45-encoded string"""
     res = ""
-    buf = list(s)
-    while buf:
-        if len(buf) >= 2:
-            x = (buf.pop(0) << 8) + buf.pop(0)
+    buflen = len(buf)
+    for i in range(0, buflen, 2):
+        if buflen - i > 1:
+            x = (buf[i] << 8) + buf[i + 1]
             e, x = divmod(x, 45 * 45)
             d, c = divmod(x, 45)
             res += BASE45_CHARSET[c] + BASE45_CHARSET[d] + BASE45_CHARSET[e]
         else:
-            x = buf.pop(0)
+            x = buf[i]
             d, c = divmod(x, 45)
             res += BASE45_CHARSET[c] + BASE45_CHARSET[d]
     return res
@@ -27,12 +27,13 @@ def b45decode(s: str) -> bytes:
     res = []
     try:
         buf = [BASE45_CHARSET.index(c) for c in s]
-        while buf:
-            if len(buf) >= 3:
-                x = buf.pop(0) + buf.pop(0) * 45 + buf.pop(0) * 45 * 45
+        buflen = len(buf)
+        for i in range(0, buflen, 3):
+            x = buf[i] + buf[i + 1] * 45
+            if buflen - i >= 3:
+                x = buf[i] + buf[i + 1] * 45 + buf[i + 2] * 45 * 45
                 res.extend(list(divmod(x, 256)))
             else:
-                x = buf.pop(0) + buf.pop(0) * 45
                 res.append(x)
         return bytes(res)
     except (ValueError, IndexError):
