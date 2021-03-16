3d285a66d31c408f5f4d44acb39dc96cf8e4ac20
Jakob Schlyter <jakob@kirei.se>
2021-03-16 08:32:15 +0100

    more

--- test/test_base45.py
+++ test/test_base45.py
@@ -14,22 +14,26 @@ GOOD_DATA = [
     ),
 ]
 
-BAD_STRINGS = ["xyzzy", "::::", "a"]
+BAD_BASE45_STRINGS = ["xyzzy", "::::", "a"]
 
 
 class TestBase45(unittest.TestCase):
-    def test_encode_simple(self):
+    def test_encode_good(self):
         for binary_ref, encoded_ref in GOOD_DATA:
             encoded_res = b45encode(binary_ref)
             self.assertEqual(encoded_res, encoded_ref)
 
-    def test_decode_simple(self):
+    def test_decode_good(self):
         for binary_ref, encoded_ref in GOOD_DATA:
             binary_res = b45decode(encoded_ref)
             self.assertEqual(binary_res, binary_ref)
 
+    def test_encode_bad(self):
+        with self.assertRaises(TypeError):
+            b45encode(42)
+
     def test_decode_bad(self):
-        for v in BAD_STRINGS:
+        for v in BAD_BASE45_STRINGS:
             with self.assertRaises(ValueError):
                 b45decode(v)
 
